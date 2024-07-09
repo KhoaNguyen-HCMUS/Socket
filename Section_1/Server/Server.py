0356140f4bf59c071a36d56c476b975dc33ca5e7
KhoaNguyen <23127211@student.hcmus.edu.vn>
2024-07-09 23:19:43 +0700

    Add folder and delete multithread in source code

--- Section_1/Server/Server.py
+++ Section_1/Server/Server.py
@@ -1,13 +1,12 @@
 import socket
 import os
-import threading
 
 def send_file_list(client_socket):
     with open('files_list.txt', 'r') as f:
         files_list = f.read()
     client_socket.sendall(files_list.encode())
 
-def send_file(client_socket, file_name, addr):
+def send_file(client_socket, file_name):
     # Prepend the "Cloud" folder to the file_name
     server_py_directory = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(server_py_directory,'Cloud', file_name)
@@ -24,25 +23,11 @@ def send_file(client_socket, file_name, addr):
                 data = f.read(1024)
                 client_socket.sendall(data)
                 bytes_sent += len(data)
-        print(f"Sent file for client {addr}: {file_name}" )
+        print(f"Sent file: {file_name}")
     else:
         # Send a response indicating the file does not exist
         client_socket.sendall("0".encode())  # Indicating file size of 0 for non-existent file
         print(f"File {file_name} not found.")
-
-def handle_client_connection(client_socket, addr):
-    print(f"-----Connection from {addr}-----")
-    try:
-        while True:
-            file_name = client_socket.recv(1024).decode()
-            if not file_name:
-                break
-            send_file(client_socket, file_name, addr)
-    except socket.error as err:
-        print(f"Socket error: {err}")
-    finally:
-        client_socket.close()
-
 def server():
     host = '192.168.1.19' # Server IP address
     #host = '127.0.0.1' #loopback address
@@ -55,9 +40,22 @@ def server():
     
     while True:
         client_socket, addr = server_socket.accept()
-        client_handler = threading.Thread(target=handle_client_connection, args=(client_socket, addr))
-        client_handler.start()
-
+        print(f"-----Connection from {addr}-----")
+        try:
+            while True:
+                file_name = client_socket.recv(1024).decode()
+                if not file_name:
+                    break
+                send_file(client_socket, file_name)
+        except socket.error as err:
+            print(f"Socket error: {err}")
+        except ConnectionError:
+            print("Connection error")
+        except TimeoutError:
+            print("Timeout")
+        finally:
+            print(f"-----Client {addr} has disconnected-----")
+            client_socket.close()
 
 if __name__ == '__main__':
     server()
