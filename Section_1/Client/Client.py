d11a21d9ec6d5294254b11fa08426a7dd5dd3fbc
KhoaNguyen <23127211@student.hcmus.edu.vn>
2024-07-23 20:44:36 +0700

    Update class and object

--- Section_1/Client/Client.py
+++ Section_1/Client/Client.py
@@ -1,75 +1,86 @@
 import socket
-import time
+import queue
 import os
 
 
-def get_file_list(client_socket):
-    client_socket.sendall("list".encode())
-    file_list = client_socket.recv(1024).decode()
-    print("File list:")
-    print(file_list)
+class Client:
+    def __init__(self):
+        self.host = "127.0.0.1"
+        self.port = 10000
+        self.client_socket = None
+        self.downloaded_files = set()
+        self.file_queue = queue.Queue()
 
+    def get_file_list(self):
+        self.client_socket.sendall("list".encode())
+        file_list = self.client_socket.recv(1024).decode()
+        print("File list:")
+        print(file_list)
 
-def get_new_files(downloaded_files):
-    with open("input.txt", "r") as f:
-        current_files = f.read().splitlines()
-    return [file for file in current_files if file not in downloaded_files]
+    def get_new_files(self):
+        with open("input.txt", "r") as f:
+            current_files = f.read().splitlines()
+        for file in current_files:
+            if file not in self.downloaded_files:
+                self.file_queue.put(file)
 
+    def request_file_download(self, file_name):
+        if file_name in self.downloaded_files:
+            print(f"Skipping {file_name}, already downloaded.\n-------------")
+            return
 
-def connect_to_server():
-    # host = '192.168.1.19' # Server IP address
-    host = "127.0.0.1"  # loopback address
-    port = 10000
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((host, port))
-    return client_socket
+        self.client_socket.sendall(file_name.encode())
+        file_size = int(self.client_socket.recv(1024).decode())
+        if file_size:
+            downloaded_size = 0
+            with open(os.path.join("Output", file_name), "wb") as f:
+                while downloaded_size < file_size:
+                    data = self.client_socket.recv(1024)
+                    f.write(data)
+                    downloaded_size += len(data)
+                    progress_percentage = (downloaded_size / file_size) * 100
+                    print(
+                        f"Downloading {file_name}: {progress_percentage:.2f}% complete",
+                        end="\r",
+                    )
+            print(f"\n-------------")
+            self.downloaded_files.add(file_name)
+        else:
+            print(f"File {file_name} not found on server\n-------------")
 
+    def process_file_queue(self):
+        while not self.file_queue.empty():
+            file_name = self.file_queue.get()
+            self.request_file_download(file_name)
 
-def request_file_download(client_socket, file_name):
-    client_socket.sendall(file_name.encode())
-    # Assume the server sends the file size first
-    file_size = int(client_socket.recv(1024).decode())
-    if file_size:
-        downloaded_size = 0  # Initialize the number of downloaded bytes
-        with open(os.path.join("output", file_name), "wb") as f:
-            while downloaded_size < file_size:
-                data = client_socket.recv(1024)
-                f.write(data)
-                downloaded_size += len(data)
-                # Calculate and display progress percentage
-                progress_percentage = (downloaded_size / file_size) * 100
-                print(
-                    f"Downloading {file_name}: {progress_percentage:.2f}% complete",
-                    end="\r",
-                )
-        print(f"\n-------------")
-    else:
-        print(f"File {file_name} not found on server\n-------------")
+    def connect_to_server(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((self.host, self.port))
+        return self.client_socket
 
-
-def main():
-    downloaded_files = set()
-    client_socket = connect_to_server()
-
-    try:
-        print("Connected to server.")
-        print("Client address:", client_socket.getsockname())
-        get_file_list(client_socket)
-        while True:
-            new_files = set(get_new_files(downloaded_files))  # Convert to set to remove duplicates
-            for file_name in new_files:
-                request_file_download(client_socket, file_name)
-                downloaded_files.add(file_name)
-
-    except ConnectionRefusedError:
-        print("Can't connect to server. Please check IP address and port again.")
-    except Exception as e:
-        print(f"Error: {e}")
-    except KeyboardInterrupt:
-        print("Client is shutting down.")
-    finally:
-        client_socket.close()
+    def run(self):
+        self.downloaded_files = set()
+        self.client_socket = self.connect_to_server()
+        try:
+            print("Connected to server.")
+            self.get_file_list()
+            self.get_new_files()
+            self.process_file_queue()
+        except ConnectionRefusedError:
+            print("Connection refused. Server is not running.")
+        except KeyboardInterrupt:
+            print("\nClient is closing...")
+            if self.client_socket:
+                self.client_socket.close()
+            print("Client closed.")
+        except Exception as e:
+            print(f"Error: {e}")
+            if self.client_socket:
+                self.client_socket.close()
+        finally:
+            if self.client_socket:
+                self.client_socket.close()
 
 
 if __name__ == "__main__":
-    main()
+    Client().run()
--- Section_1/Server/Server.py
+++ Section_1/Server/Server.py
@@ -1,74 +1,65 @@
 import socket
 import os
-
-def send_file_list(client_socket: socket.socket):
-    message = client_socket.recv(1024).decode()
-    if message == "list":
-        handle_file_list(client_socket, "files_list.txt")
+import signal
 
 
-def handle_file_list(client_socket, file_name):
-    with open("files_list.txt", "r") as f:
-        files_list = f.read()
-    client_socket.sendall(files_list.encode())
+class Server:
+    def __init__(self):
+        self.host = "127.0.0.1"
+        self.port = 10000
 
+    def send_file_list(self, client_socket: socket.socket):
+        message = client_socket.recv(1024).decode()
+        if message == "list":
+            self.handle_file_list(client_socket, "files_list.txt")
 
-def send_file(client_socket, file_name):
-    # Prepend the "Cloud" folder to the file_name
-    server_py_directory = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(server_py_directory, "Cloud", file_name)
-    # Initialize file_size
-    file_size = None
+    def handle_file_list(self, client_socket, file_name):
+        with open("files_list.txt", "r") as f:
+            files_list = f.read()
+        client_socket.sendall(files_list.encode())
 
-    # Open files_list.txt and read file sizes
-    if os.path.exists(file_path):
-        file_size = os.path.getsize(file_path)
-        client_socket.sendall(str(file_size).encode())
-        with open(file_path, "rb") as f:
-            bytes_sent = 0
-            while bytes_sent < file_size:
-                data = f.read(1024)
-                client_socket.sendall(data)
-                bytes_sent += len(data)
-        print(f"Sent file: {file_name}")
-    else:
-        # Send a response indicating the file does not exist
-        client_socket.sendall(
-            "0".encode()
-        )  # Indicating file size of 0 for non-existent file
-        print(f"File {file_name} not found.")
+    def send_file(self, client_socket: socket.socket, file_name):
+        # Prepend the "Cloud" folder to the file_name
+        server_py_directory = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(server_py_directory, "Cloud", file_name)
+        # Initialize file_size
+        file_size = None
 
+        # Open files_list.txt and read file sizes
+        if os.path.exists(file_path):
+            file_size = os.path.getsize(file_path)
+            client_socket.sendall(str(file_size).encode())
+            with open(file_path, "rb") as f:
+                bytes_sent = 0
+                while bytes_sent < file_size:
+                    data = f.read(1024)
+                    client_socket.sendall(data)
+                    bytes_sent += len(data)
+            print(f"Sent file: {file_name}")
+        else:
+            # Send a response indicating the file does not exist
+            client_socket.sendall("0".encode())
 
-def server():
-    # host = '192.168.1.19' # Server IP address
-    host = "127.0.0.1"  # loopback address
-    port = 10000
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # IPv4, TCP
-    server_socket.bind((host, port))
-    server_socket.listen(5)
-    print(f"Server started on {host} port:{port}")
-    print("Server is listening...")
-
-    while True:
-        client_socket, addr = server_socket.accept()
-        print(f"-----Connection from {addr}-----")
-        send_file_list(client_socket)
-        try:
-            while True:
-                file_name = client_socket.recv(1024).decode()
-                if not file_name:
-                    break
-                send_file(client_socket, file_name)
-        except socket.error as err:
-            print(f"Socket error: {err}")
-        except ConnectionError:
-            print("Connection error")
-        except TimeoutError:
-            print("Timeout")
-        finally:
-            print(f"-----Client {addr} has disconnected-----")
-            client_socket.close()
+    def run(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(5)
+        print("Server started. Waiting for connection...")
+        while True:
+            client_socket, addr = self.server_socket.accept()
+            print(f"Connection from {addr}")
+            self.send_file_list(client_socket)
+            try:
+                while True:
+                    file_name = client_socket.recv(1024).decode()
+                    if not file_name:
+                        break
+                    self.send_file(client_socket, file_name)
+            except Exception as e:
+                print(f"Error: {e}")
+            finally:
+                client_socket.close()
 
 
 if __name__ == "__main__":
-    server()
+    Server().run()
