36db8713c9a841115b33f31ab529c171e5312e80
callmezuy <mduy2204@gmail.com>
2024-07-26 14:43:20 +0700

    update Section 3

--- Section_3/Client/Client.py
+++ Section_3/Client/Client.py
@@ -1,146 +1,74 @@
 import os
 import socket
 import sys
-import time
 
+def get_file_list(client_socket: socket.socket):
+    client_socket.sendall("LIST".encode())
+    file_list = client_socket.recv(1024).decode()
+    for file in file_list.strip().split("\n"):
+        file_name, file_size = file.split(SEPARATOR)
+        print(file_name + " " + getStandardSize(int(file_size)))
 
-# def read_input_file():
-#     with open("input.txt", "r") as file:
-#         return [tuple(line.strip().split(",")) for line in file.readlines()]
+def getStandardSize(size):
+    itme=['B','KB','MB','GB','TB']
+    for x in itme:
+        if size < 1024.0:
+            return  "%3.1f%s" % (size,x)
+        size/=1024.0
+    return size
 
-PRIOR_MAP = {"CRITICAL": 10, "HIGH": 4, "LOW": 1}
-
-
-def get_priority_size(file_priority):
-    return 1024 * PRIOR_MAP.get(file_priority, 1)
-
-
-def get_input_files():
+def read_input_files():
     with open("input.txt", "r") as f:
-        input_file_list_origin = f.read().splitlines()
-        input_file_list_origin = [file.split(" ") for file in input_file_list_origin]
+        input_files = f.read().split("\n")
+        input_files = [file.split(SEPARATOR) for file in input_files]
 
     return [
         (file_name, get_priority_size(file_priority))
-        for file_name, file_priority in input_file_list_origin
+        for file_name, file_priority in input_files
     ]
 
+PRIOR_MAP = {"CRITICAL": 10, "HIGH": 4, "LOW": 1}
+
+def get_priority_size(file_priority):
+    return 1024 * PRIOR_MAP.get(file_priority, 1)
 
 def write_file(file_name, data):
     with open(os.path.join("Output", file_name), "ab") as f:
         f.write(data)
 
-
-def manage_file(file_path, file_size, client_socket):
-    downloaded_size = 0
-    os.makedirs("output", exist_ok=True)
-    with open(os.path.join("output", file_path), "wb") as f:
-        while downloaded_size < file_size:
-            data = client_socket.recv(1024)
-            f.write(data)
-            downloaded_size += len(data)
-            percent = 100.0 * downloaded_size / file_size
-            bar_length = 30
-            sys.stdout.write(
-                f"\r{file_path:<20} [{'=' * int(bar_length * percent / 100):<30}] {percent:.2f}%"
-            )
-            sys.stdout.flush()
-    print("\nDownload complete")
-
-
-def request_file_download(client_socket: socket.socket, file_name, priority):
-    pass
-
-
 SEPARATOR = " "
 
-
 def monitor_input_file(client_socket: socket.socket):
-    client_socket.sendall("VALID_LIST".encode())
-    raw_valid_files = client_socket.recv(1024).decode()
-    valid_files: dict = {}
+    pass
 
-    for file in raw_valid_files.split("\n"):
-        file_name, file_size = file.split(SEPARATOR)
-        file_size = int(file_size)
-
-        valid_files[file_name] = file_size
-
-    input_files = set(get_input_files())
-    num_input_files = len(input_files)
-
-    downloaded_files = set()
-    num_downloaded_files = 0
-
-    downloader = {}
-
-    print("input", input_files)
-    print("valid", valid_files)
-
-    print("Monitoring input files...")
-    #while True:
-    client_socket.sendall("get".encode())
-
-    for file_name, priority_size in input_files:
-        if file_name in valid_files and file_name not in downloaded_files:
-            print("send", file_name, priority_size)
-            client_socket.sendall(f"GET{SEPARATOR}{file_name}{SEPARATOR}{priority_size}".encode())
-            
-            # receive data
-            data = client_socket.recv(1024).decode()
-            print("recv", data)
-            _, filename, chunk_data = data.split(SEPARATOR)
-            #_, filename, chunk_data = "", "", b""
-            print(_, filename, chunk_data)
-            # process data, if data is eof, then update downloaded_files
-            if chunk_data == "eof":
-                downloaded_files.add(file_name)
-                num_downloaded_files += 1
-            else:
-                write_file(file_name, chunk_data)
-
-            #if num_downloaded_files == num_input_files:
-                #break
-            client_socket.sendall("get".encode())
-        else:
-            print(f"File {file_name} not found")
-            client_socket.sendall("continue".encode())
-            client_socket.sendall("get".encode())
-        if num_downloaded_files == num_input_files:
-            break
-
-        # input_files.update(get_input_files())s
-
-
-def get_file_list(client_socket: socket.socket):
-    client_socket.sendall("LIST".encode())
-    file_list = client_socket.recv(1024).decode()
-    print("File list:")
-    print(file_list)
-
+def client_request(client_socket: socket.socket):
+    get_file_list(client_socket)
+    input_files = read_input_files(input_files)
+    
 
 def main():
     host = "127.0.0.1"
-    port = 10000
+    port = 65432
+
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     client_socket.connect((host, port))
 
-    try:
-        get_file_list(client_socket)
-        monitor_input_file(client_socket)
-    except ConnectionRefusedError:
-        print("Connection refused. Server is not running.")
+    try: 
+        client_request(client_socket)
+
     except KeyboardInterrupt:
         print("\nClient is closing...")
         client_socket.close()
         print("Client closed.")
+    except ConnectionRefusedError:
+        print("Connection refused. Server not responding.")
     except Exception as e:
         print(f"Error: {e}")
         if client_socket:
             client_socket.close()
     finally:
         client_socket.close()
-
 
 if __name__ == "__main__":
     main()
--- Section_3/Server/Server.py
+++ Section_3/Server/Server.py
@@ -2,9 +2,22 @@ import os
 import socket
 import threading
 
+def gen_file_list(file_path):
+    with open(file_path, "w") as f:
+        for file_name in os.listdir("Cloud"):
+            f.write(f'{file_name}{SEPARATOR}{str(os.path.getsize(os.path.join("Cloud", file_name)))}\n')
+
+def send_file_list(client_socket: socket.socket):
+    msg = client_socket.recv(1024).decode()
+    if msg == "LIST":
+        with open("file_list.txt", "r") as f:
+            file_list = f.read()
+        client_socket.sendall(file_list.encode())
+    else:
+        print("\nInvalid command.")
 
 def file_chunk_generator(file_path, chunk_size):
-    #with open(file_path, "rb") as f:
+    # with open(file_path, "rb") as f:
     #   print(f.read())
     with open(file_path, "rb") as f:
         while True:
@@ -13,85 +26,30 @@ def file_chunk_generator(file_path, chunk_size):
                 break
             yield data
 
-
-def send_file_list(client_socket: socket.socket):
-    msg = client_socket.recv(1024).decode()
-    if msg == "LIST":
-        with open("files_list.txt", "r") as f:
-            files_list = f.read()
-        client_socket.sendall(files_list.encode())
-    else:
-        client_socket.sendall("Invalid command".encode())
-
-
 SEPARATOR = " "
 
-
-def handle_client(client_socket: socket.socket, file_list):
+def handle_client(client_socket: socket.socket):
     send_file_list(client_socket)
-    try:
-        file_list_str = "\n".join(
-            [
-                f"{file_name}{SEPARATOR}{file_size}"
-                for file_name, file_size, *_ in file_list
-            ]
-        )
-        msg=client_socket.recv(1024).decode()
-        if msg == "VALID_LIST":
-            client_socket.sendall(file_list_str.encode()) #valid file list
-            print ("valid file list sent")
-
-        manager = {}
-
-        while True:
-            cmd = client_socket.recv(1024).decode()  # get
-            if cmd == "get":
-                #print("\n get")
-                msg = client_socket.recv(1024).decode()
-                if msg == "continue":
-                    continue
-                #print(msg)
-                _, filename, priority_size = msg.split(SEPARATOR)[:3]
-                print(_, filename, priority_size)
-
-                chunk_sz = priority_size
-
-                if filename not in manager:
-                    file_path = os.path.join("Cloud", filename)
-                    manager[filename] = file_chunk_generator(file_path, chunk_sz)
-
-                chunk_data = next(manager[filename])
-                print("len", len(chunk_data))
-
-                client_socket.sendall(f"DATA{SEPARATOR}{filename}{SEPARATOR}{(chunk_data if chunk_data else "eof")}".encode())
-
-            elif cmd == "terminated":
-                break
-    except:
-        print("Error")
-    finally:
-        client_socket.close()
-
 
 def main():
     host = "127.0.0.1"
-    port = 10000
+    port = 65432
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     server_socket.bind((host, port))
     server_socket.listen(5)
+
     print("Server listening on port", port)
 
-    file_list = [
-        (f, os.path.getsize(os.path.join("Cloud", f)), os.path.join("Cloud", f))
-        for f in os.listdir("Cloud")
-    ]
+    gen_file_list("file_list.txt")
 
     while True:
         client_socket, addr = server_socket.accept()
         print("Connection from", addr)
-        handle_client(client_socket, file_list)
+        handle_client(client_socket)
+        #handle_client(client_socket, file_list)
         #threading.Thread(target=handle_client, args=(client_socket, file_list)).start()
-
 
 if __name__ == "__main__":
     main()
