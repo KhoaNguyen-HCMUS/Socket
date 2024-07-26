7ce3f81b3d798e049139319431c2cccb28bdafff
KhoaNguyen <23127211@student.hcmus.edu.vn>
2024-07-26 13:53:25 +0700

    Update Section_1

--- Section_1/Client/Client.py
+++ Section_1/Client/Client.py
@@ -3,6 +3,7 @@ import os
 import customtkinter
 import threading
 from customtkinter import *
+import math
 
 
 class Client:
@@ -14,6 +15,7 @@ class Client:
         self.text_widget = None  # Placeholder for the text widget
         self.client_running = False  # Flag to check if server is running
         self.root = customtkinter.CTk()
+        self.file_list_from_server = []
 
         # Tạo CTkLabel để hiển thị phần trăm hoàn thành
         self.progress_label = customtkinter.CTkLabel(
@@ -34,16 +36,42 @@ class Client:
 
     def get_file_list(self):
         self.client_socket.sendall("list".encode())
-        file_list = self.client_socket.recv(1024).decode()
-        self.log_message("File list:")
-        self.log_message(file_list)
+        file_list_str = self.client_socket.recv(1024).decode()
+        file_list = file_list_str.strip().split("\n")
+        file_names = []
+        processed_files = []
+
+        def convert_size(size_bytes):
+            if size_bytes == 0:
+                return "0B"
+            size_name = ("B", "KB", "MB", "GB", "TB")
+            i = int(math.floor(math.log(size_bytes, 1024)))
+            p = math.pow(1024, i)
+            s = round(size_bytes / p, 2)
+            return f"{s} {size_name[i]}"
+
+        for file in file_list:
+            name, size = file.split(" ")
+            size = int(size)
+            readable_size = convert_size(size)
+            file_names.append(name)
+            processed_files.append(f"{name} - {readable_size}")
+
+        self.file_list_from_server = file_names
+        self.log_message(f"Files available:\n {'\n'.join(file_names)}")
+        # self.log_message(f"Files available on server:\n {'\n'.join(processed_files)}")
 
     def get_new_files(self):
         with open("input.txt", "r") as f:
             current_files = f.read().splitlines()
         return [file for file in current_files if file not in self.downloaded_files]
 
-    def request_file_download(self, file_name):
+    def request_file_download(self, file_name, count_appear):
+        if file_name in self.downloaded_files:
+            self.log_message(
+                f"File {file_name} has already been downloaded.\n-------------"
+            )
+            return
         self.client_socket.sendall(file_name.encode())
         # Assume the server sends the file size first
         file_size = int(self.client_socket.recv(1024).decode())
@@ -73,8 +101,13 @@ class Client:
                     self.root.update()
             # print("Download complete")
             self.log_message(f"\n Download {file_name} successful\n-------------")
+
         else:
-            self.log_message(f"File {file_name} not found on server\n-------------")
+            (
+                self.log_message(f"File {file_name} not found on server\n-------------")
+                if (count_appear[file_name] == 0)
+                else None
+            )
 
     def connect_to_server(self):
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -82,7 +115,6 @@ class Client:
         return self.client_socket
 
     def run(self):
-        self.downloaded_files = set()
         try:
             self.client_socket = self.connect_to_server()
             self.client_socket.connect((self.host, self.port))
@@ -90,15 +122,25 @@ class Client:
                 self.log_message(
                     "Connection refused. Server is not running. Please stop client and try again later."
                 )
-
             print("Connected to server")
             self.get_file_list()
+            count_appear = {}
             while not self.client_running:
                 new_files = self.get_new_files()
+                list_not_exist = [
+                    file for file in new_files if file not in self.file_list_from_server
+                ]
                 for file_name in new_files:
-                    if file_name not in self.downloaded_files:
-                        self.request_file_download(file_name)
-                        self.downloaded_files.add(file_name)
+                    if file_name in self.downloaded_files:
+                        self.log_message(f"File {file_name} already downloaded")
+                    else:
+                        if file_name not in count_appear:
+                            count_appear[file_name] = 0
+                        self.request_file_download(file_name, count_appear)
+                        if file_name not in list_not_exist:
+                            self.downloaded_files.add(file_name)
+                        count_appear[file_name] += 1
+
         except KeyboardInterrupt:
             self.log_message("\nClient is closing...")
             if self.client_socket:
@@ -126,8 +168,12 @@ class Client:
 
     def input_file_name(self):
         file_name = self.file_input_entry.get()
+        if file_name not in self.file_list_from_server:
+            self.log_message(f"{file_name} is not found on Server\n-------------")
+        if file_name in self.downloaded_files:
+            self.log_message(f"{file_name} is already downloaded\n-------------")
         with open("input.txt", "a") as f:
-            f.write(file_name + "\n")
+            f.write("\n" + file_name)
 
     def GUI(self):
         self.root = customtkinter.CTk()
